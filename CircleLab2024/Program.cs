90897a76805bf2474684e54a2cbd8e2e9a515706
MacobJagyar <jacobmagyar95@gmail.com>
2024-03-22 15:02:41 -0400

    First push

---
+++ CircleLab2024/Program.cs
@@ -0,0 +1,50 @@
+﻿using CircleLab2024;
+
+circle circle1 = new circle(0);
+
+while (true)
+{
+    Console.Write("Please enter the radius of the circle: ");
+    if (double.TryParse(Console.ReadLine(), out double input))
+    {
+        circle1.radius = input;
+
+        Console.WriteLine($"Diameter: {circle1.CalcDiameter()}" +
+            $"\nCircumference: {circle1.CalcCircumference()}" +
+            $"\nArea: {circle1.CalcArea()}");
+        break;
+    }
+
+    else
+    {
+        Console.WriteLine("Not valid input. Try again.");
+        Thread.Sleep(1500);
+        Console.Clear();
+    }
+}
+
+while (true)
+{
+    Console.Write("\nShould the circle grow? (y/n) ");
+    string growChoice = Console.ReadLine().ToLower();
+
+    if (growChoice == "y")
+    {
+        Console.WriteLine("\nThe circle is magically growing...");
+        circle1.Grow();
+        Console.WriteLine($"\nDiameter: {circle1.CalcDiameter()}" +
+    $"\nCircumference: {circle1.CalcCircumference()}" +
+    $"\nArea: {circle1.CalcArea()}");
+    }
+    else if (growChoice == "n")
+    {
+        Console.WriteLine($"\nGoodbye. The circle's final radius is {circle1.GetRadius()}");
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"\nThat wasn't y or n. Assuming you want to stop. The final radius is {circle1.GetRadius()}");
+        break;
+    }
+}
+
---
+++ CircleLab2024/circle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleLab2024
+{
+    public class circle
+    {
+        public circle(double aRadius) 
+        {
+            radius = aRadius;
+        }
+
+        public double radius { get; set; }
+
+        public double CalcDiameter()
+        {
+            return radius * 2;
+        }
+
+        public double CalcCircumference()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public double CalcArea()
+        {
+            return Math.PI * (Math.Pow(radius, 2));
+        }
+
+        public void Grow()
+        {
+            radius *= 2;
+        }
+
+        public double GetRadius()
+        {
+            return radius;
+        }
+    }
+}
